b092ed1e7a29ecfe72dea60e85f8e2330f07bc68
RehamMobarak <reham.a.mobarak@gmail.com>
2020-12-26 06:23:01 +0200

    configuring and installing express packages

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-// Setup empty JS object to act as endpoint for all routes
+//JS object to hold all routes
 projectData = {};
 
-// Require Express to run server and routes
+//Initializing server configs
+// install Express
+const express = require("express");
+// making an app instance
+const app = express();
 
-// Start up an instance of app
+//configuring bodyParser as middle-ware
+const body_parser = require("body-parser");
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// and Cors to allow cross origin
+const cors = require("cors");
+app.use(cors());
 
-// Cors for cross origin allowance
+// define project folder
+app.use(express.static("website"));
 
-// Initialize the main project folder
-app.use(express.static('website'));
+// Setting up the server
+const port = 5000;
+const server = app.listen(port, () => {
+  console.log(`Hi, we're running on localhost: ${port}`);
+});
 
-
-// Setup Server
