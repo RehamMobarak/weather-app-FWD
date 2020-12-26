db8fd50995964de7b1d27298ef869956e824aa9a
RehamMobarak <reham.a.mobarak@gmail.com>
2020-12-26 07:28:31 +0200

    Function for getting - fetching- data from the API

--- website/app.js
+++ website/app.js
@@ -9,4 +9,21 @@ let currentDate = day + "." + month + "." + year;
 let apiUrl = "http://api.openweathermap.org/data/2.5/weather?zip=";
 let k = ",us&appid=b881ca4d0f76a58889e9aea0e56e2fca&units=metric";
 
+//make an event listener on the button -click- to call functions that GET data from the api, add user feelings and POST it then show it in Most recent entry section on the page
+
+//zip code examples:94040,85005
+const btn = document.getElementById("generate");
+btn.addEventListener("click", generateBtn);
+
+// Function for getting data from the weather API
+const getData = async (apiUrl, zCode, k) => {
+    const response = await fetch(apiUrl + zCode + k);
+    try {
+      const fetchedData = await response.json();
+      console.log(fetchedData);
+      return fetchedData;
+    } catch (error) {
+      console.log("Sorry, there is an error..!", error);
+    }
+  };
 
