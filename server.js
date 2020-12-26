48af05ba1debf223ad74ca94190232195a2e946e
RehamMobarak <reham.a.mobarak@gmail.com>
2020-12-26 06:36:38 +0200

    get data url

--- server.js
+++ server.js
@@ -1,27 +1,39 @@
 //JS object to hold all routes
-projectData = {};
+myData = {};
 
 //Initializing server configs
 // install Express
 const express = require("express");
 // making an app instance
-const app = express();
+const myapp = express();
 
 //configuring bodyParser as middle-ware
 const body_parser = require("body-parser");
-app.use(body_parser.urlencoded({ extended: false }));
-app.use(body_parser.json());
+myapp.use(body_parser.urlencoded({ extended: false }));
+myapp.use(body_parser.json());
 
 // and Cors to allow cross origin
 const cors = require("cors");
-app.use(cors());
+myapp.use(cors());
 
 // define project folder
-app.use(express.static("website"));
+myapp.use(express.static("website"));
 
 // Setting up the server
 const port = 5000;
-const server = app.listen(port, () => {
+const server = myapp.listen(port, () => {
   console.log(`Hi, we're running on localhost: ${port}`);
 });
+
+//defining home page url
+myapp.get("/", function (req, res) {
+    res.send("index.html");
+  });
+
+  //defining get data url
+  myapp.get("/myData", getmyData);
+function getmyData(req, res) {
+  res.send(myData);
+  console.log(myData);
+}
 
