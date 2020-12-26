79f70cbef1c52f6e4e856db26616fa4ef6db9f52
RehamMobarak <reham.a.mobarak@gmail.com>
2020-12-26 07:37:17 +0200

    generate Btn action

--- website/app.js
+++ website/app.js
@@ -11,9 +11,8 @@ let k = ",us&appid=b881ca4d0f76a58889e9aea0e56e2fca&units=metric";
 
 //make an event listener on the button -click- to call functions that GET data from the api, add user feelings and POST it then show it in Most recent entry section on the page
 
-//zip code examples:94040,85005
 const btn = document.getElementById("generate");
-btn.addEventListener("click", generateBtn);
+btn.addEventListener("click", generateButton);
 
 // Function for getting data from the weather API
 const getData = async (apiUrl, zCode, k) => {
@@ -26,4 +25,27 @@ const getData = async (apiUrl, zCode, k) => {
       console.log("Sorry, there is an error..!", error);
     }
   };
+
+
+//what to do after clicking generate button
+//take country zip code from input to make a request with it and GET weather data from the api
+//zip code examples:94040,85005
+//then take user feelings from the input and POST it with the request
+function generateButton(e) {
+    const zCode = document.getElementById("zip").value;
+    const userFeelings = document.getElementById("feelings").value;
+    getData(apiUrl, zCode, k).then((fetchedData) => {
+      dataPOST("/addProjectData", {
+        temperature: fetchedData.main.temp,
+        date: currentDate,
+        userContent: userFeelings,
+      }).then(() => {
+        update();
+      });
+    });
+  }
+
+
+
+
 
