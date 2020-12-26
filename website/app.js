6c7a6109669b01a60d1af3b452f40d359bd63432
RehamMobarak <reham.a.mobarak@gmail.com>
2020-12-26 09:20:32 +0200

    handling empty zip or feeling input, and if zip code city not found

--- website/app.js
+++ website/app.js
@@ -16,24 +16,35 @@ btn.addEventListener("click", generateButton);
 
 // Function for getting data from the weather API
 const getData = async (apiUrl, zCode, k) => {
-    const response = await fetch(apiUrl + zCode + k);
-    try {
-      const fetchedData = await response.json();
+  const response = await fetch(apiUrl + zCode + k);
+  try {
+    const fetchedData = await response.json();
+    if (fetchedData.message == undefined) {
       console.log(fetchedData);
       return fetchedData;
-    } catch (error) {
-      console.log("Sorry, there is an error..!", error);
+    } else {
+      alert(fetchedData.message);
     }
-  };
-
+  } catch (error) {
+    console.log("Sorry, there is an error..!", error);
+  }
+};
 
 //what to do after clicking generate button
 //take country zip code from input to make a request with it and GET weather data from the api
 //zip code examples:94040,85005
 //then take user feelings from the input and POST it with the request
 function generateButton(e) {
-    const zCode = document.getElementById("zip").value;
-    const userFeelings = document.getElementById("feelings").value;
+  const zCode = document.getElementById("zip").value;
+  const userFeelings = document.getElementById("feelings").value;
+  if (zCode == "") {
+    alert("YOU MUST ENTER ZIPCODE!");
+  }
+
+  if (userFeelings == "") {
+    alert("YOU MUST ENTER FEELINGS!");
+  }
+  if (zCode && userFeelings !== "") {
     getData(apiUrl, zCode, k).then((fetchedData) => {
       dataPOST("/addProjectData", {
         temperature: fetchedData.main.temp,
@@ -46,57 +57,56 @@ function generateButton(e) {
       });
     });
   }
+}
 
 //POST desired fetched data parts which we got from the api GET request with user response -feelings- input
 const dataPOST = async (url = "", feelingInput = {}) => {
-    // console.log(feelingInput);
-    const res = await fetch(url, {
-      method: "POST",
-      credentials: "same-origin",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body data type must match "Content-Type" header
-      body: JSON.stringify(feelingInput),
-    });
+  // console.log(feelingInput);
+  const res = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Body data type must match "Content-Type" header
+    body: JSON.stringify(feelingInput),
+  });
 
-    try {
-      const newEntry = await res.json();
+  try {
+    const newEntry = await res.json();
     //   console.log(newEntry);
-      return newEntry;
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
+    return newEntry;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
 //UPDATE the page with the new data that includes fetched data from the api - temp - and user input - feelings - and date -generated instantly-
 const update = async () => {
-    const request = await fetch("/myData");
-    try {
-      const result = await request.json();
+  const request = await fetch("/myData");
+  try {
+    const result = await request.json();
     //   console.log(result);
-      const UIdate = document.getElementById("date");
-      UIdate.innerHTML = "Date: " + result.date;
+    const UIdate = document.getElementById("date");
+    UIdate.innerHTML = "Date: " + result.date;
 
-      const UItemp = document.getElementById("temp");
-      UItemp.innerHTML = "Temperature: " + result.temperature;
+    const UItemp = document.getElementById("temp");
+    UItemp.innerHTML = "Temperature: " + result.temperature;
 
-      const UIfeel = document.getElementById("content");
-      UIfeel.innerHTML = "Your Feeling: " + result.userContent;
+    const UIfeel = document.getElementById("content");
+    UIfeel.innerHTML = "Your Feeling: " + result.userContent;
 
-      const UIcityName = document.getElementById("cityName");
-      UIcityName.innerHTML = "City Name: " + result.name;
+    const UIcityName = document.getElementById("cityName");
+    UIcityName.innerHTML = "City Name: " + result.name;
 
-      const UIwindSpeed = document.getElementById("windSpeed");
-      UIwindSpeed.innerHTML = "Wind Speed: " + result.windSpeed;
+    const UIwindSpeed = document.getElementById("windSpeed");
+    UIwindSpeed.innerHTML = "Wind Speed: " + result.windSpeed;
 
-      //emptying inputs after submit
-      document.getElementById("zip").value ="";
+    //emptying inputs after submit
+    document.getElementById("zip").value = "";
 
-      document.getElementById("feelings").value ="";
-
-    } catch (error) {
-      console.log("Sorry, it's an error..!", error);
-    }
-  };
-
+    document.getElementById("feelings").value = "";
+  } catch (error) {
+    console.log("Sorry, it's an error..!", error);
+  }
+};
